fbc8c97cd47857344bb73b084a957291dabbc64e
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-08 14:19:08 +0200

    adding header and variables

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="header col-12 d-flex align-items-center">
+        <img src="../assets/spotify-logo.png" alt="spotify logo" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/variables";
+.header {
+  background-color: $bg-secondary;
+  height: 70px;
+  img {
+    height: 50px;
+  }
+}
+</style>
